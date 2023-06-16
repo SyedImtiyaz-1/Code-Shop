d5482592c7e37f06c841c08e1066b7aa175d0803
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-06-17 01:36:00 +0530

    addded copyright {clickable}

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let heroSec = document.querySelector("#hero img");
-let scroll = document.querySelector(".mouse_scroll")
+let scroll = document.querySelector(".mouse_scroll");
+let copyright = document.querySelector("#copyright u");
 
 heroSec.addEventListener("click", function () {
   console.log("I am a click event listener");
@@ -9,5 +10,8 @@ scroll.addEventListener("click", function(){
     window.scrollBy(0, 980);
 })
 
-
+// github copyright onclick
+copyright.addEventListener("click", ()=>{
+  window.open("https://github.com/SyedImtiyaz-1");
+})
 
