4d42aeedce55a4791265216e1600e4eb7f5ccfab
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-06-17 23:00:11 +0530

    mail functionality

--- js/script.js
+++ js/script.js
@@ -3,24 +3,33 @@ let scroll = document.querySelector(".mouse_scroll");
 let copyright = document.querySelector("#copyright u");
 let input = document.querySelector(".mail-part-1 input");
 let mailBtn = document.querySelector(".mail-part-1 button");
+// let placeholder = document.querySelector(".mail-part-1 input placeholder");
 
 heroSec.addEventListener("click", function () {
   console.log("I am a click event listener");
 });
 
-scroll.addEventListener("click", function(){
-    window.scrollBy(0, 980);
-})
+scroll.addEventListener("click", function () {
+  window.scrollBy(0, 980);
+});
 
 // github copyright onclick
-copyright.addEventListener("click", ()=>{
+copyright.addEventListener("click", () => {
   window.open("https://github.com/SyedImtiyaz-1");
-})
+});
 
-// mail 
-mailBtn.addEventListener("click", ()=>{
-  if(mailBtn){
-    input.value=" ";
-    alert("Subscribed !")
+// mail
+mailBtn.addEventListener("click", () => {
+  if (input.required == true) {
+    alert("Subscribed !");
+    input.value = " ";
+  } else if ((input.value = " ")) {
+    setTimeout(() => {
+      input.placeholder = "Please enter email first";
+    });
   }
-})
+
+  setTimeout(() => {
+    input.placeholder = "Enter your mail";
+  }, 600);
+});
