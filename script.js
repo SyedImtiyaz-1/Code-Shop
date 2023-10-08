6c4d3ed81facd83b2a74f3acb8a2898b8dfaab26
Logesh-waran2003 <logeshwaransrini@gmail.com>
2023-10-08 15:00:02 +0530

    Added Back to top button on Homepage

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Get the button:
+let mybutton = document.getElementById("goToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
