8e6c0edfb840d94c5976a0c41bfa7f15f6b587e5
Sushil Verma <114687502+Sushilverma002@users.noreply.github.com>
2023-10-11 05:15:17 +0530

    frequently asked questions are added

--- script.js
+++ script.js
@@ -31,3 +31,20 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+//FAQS
+  // select all accordion items
+const accItems = document.querySelectorAll(".accordion__item");
+
+// add a click event for all items
+accItems.forEach((acc) => acc.addEventListener("click", toggleAcc));
+
+function toggleAcc() {
+  // remove active class from all items exept the current item (this)
+  accItems.forEach((item) => item != this ? item.classList.remove("accordion__item--active") : null
+  );
+
+  // toggle active class on current item
+  if (this.classList != "accordion__item--active") {
+    this.classList.toggle("accordion__item--active");
+  }
+}
